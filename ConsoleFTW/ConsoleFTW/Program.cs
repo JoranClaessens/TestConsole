f4e4773f7545a1ea763fdd8573e19dfa9a923a5b
Joran Claessens <11400919@student.pxl.be>
2017-01-09 09:52:53 +0100

    Initialise project

---
+++ ConsoleFTW/ConsoleFTW/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleFTW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // First program
+            Console.WriteLine("First project");
+            Console.WriteLine("Extra feature");
+        }
+    }
+}
